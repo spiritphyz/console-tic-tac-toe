700574003e17ce32ad08d2ab7bce19275c76e05c
spiritphyz <tonyle@ocf.berkeley.edu>
2016-12-05 15:21:06 -0800

    Add function to draw a board
    - Handles empty slot, 'x' mark, 'o' mark

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,58 +1,71 @@
 /*
-[ 1 0 0],
-[ 0 1 0],
-[ 0 0 1],
+  The game board is a single array that has 9 positions.
+  For each position, the values represent:
+   0: no mark
+   1: 'x' mark
+   2: 'o' mark
 
-winning combinations =
-3 mutiple
-0 3 6 = vertical column 0
-0 4 8 = diagonal from position 0
-1 4 7 = vertical column 1
-2 5 8 = vertical column 2
+   A winning combination looks like:
+  [ 1 0 0],
+  [ 0 1 0],
+  [ 0 0 1],
 
-2 multiple
-2 4 6 = diagonal from position 2
+  winning combinations =
+  3 mutiple
+  0 3 6 = vertical column 0
+  0 4 8 = diagonal from position 0
+  1 4 7 = vertical column 1
+  2 5 8 = vertical column 2
 
-1 multiple
-0 1 2 = horizontal row 0
-2 3 4 = horizontal row 1
-5 6 7 = horizontal row 2
+  2 multiple
+  2 4 6 = diagonal from position 2
+
+  1 multiple
+  0 1 2 = horizontal row 0
+  2 3 4 = horizontal row 1
+  5 6 7 = horizontal row 2
 */
 
-const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const board = [0, 2, 1,
+               2, 1, 0,
+               1, 2, 0];
 
 const drawBoard = () => {
   let drawing = '';
+  let mark = '';
   for (const [index, value] of board.entries()) {
+    switch (value) {
+    case 0:
+      drawing += ' -';
+      break;
+    case 1:
+      drawing += ' x';
+      break;
+    case 2:
+      drawing += ' o';
+      break;
+    }
     if (index === 2 || 
         index === 5 ||
         index === 8 ) {
       drawing += '\n';
     }
-    switch (value) {
-    case 0:
-      //;
-      break;
-    case 1:
-      //;
-      break;
-    case 2:
-      //;
-      break;
-    }
   }
+  console.log(drawing);
 };
 
-const readline = require('readline');
-let currentAnswer = 'default';
+drawBoard();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline');
+// let currentAnswer = 'default';
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  currentAnswer = answer;
-  console.log('Thank you for your valuable feedback:', currentAnswer);
-  rl.close();
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   currentAnswer = answer;
+//   console.log('Thank you for your valuable feedback:', currentAnswer);
+//   rl.close();
+// });
